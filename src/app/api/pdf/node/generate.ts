0652bd5790df6bf992fc3421df246b81045fe44d
Porty <portykaiden@gmail.com>
2026-04-08 00:01:08 +0200

    Remove old generate.ts causing build errors

--- src/app/api/pdf/node/generate.ts
+++
@@ -1,8 +0,0 @@
-import { renderToStream } from "@react-pdf/renderer";
-import ItinerarioPDF from "@/pdf/ItinerarioPDF";
-
-export async function generatePDFNode(titulo: string, resumen: string) {
-  return await renderToStream(
-    <ItinerarioPDF titulo={titulo} resumen={resumen} />
-  );
-}
